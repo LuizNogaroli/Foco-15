4b92a0ce031dd4bc28a798b4dddf565d09d1d0c7
Luiz Nogaroli <seu-email@exemplo.com>
2026-07-23 15:28:24 -0300

    Forca os links do Vite para https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+        
         Paginator::defaultView('vendor.pagination.custom');
     }
 }
