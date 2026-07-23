b4b60ef0bd35c91447e2f5566654db11bf8e55b8
Luiz Nogaroli <seu-email@exemplo.com>
2026-07-23 14:21:05 -0300

    Adiciona dockerignore e arruma a ordem do start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 # Sair imediatamente se algum comando falhar
 set -e
 
-echo "Limpando caches do Laravel..."
-php artisan optimize:clear
-
 echo "Rodando as migrations do banco de dados..."
 # O --force é necessário em produção para não pedir confirmação
 php artisan migrate --force
+
+echo "Limpando caches do Laravel..."
+php artisan optimize:clear
 
 echo "Iniciando o servidor Apache..."
 # Inicia o Apache no modo foreground (primeiro plano) para o container não fechar
